b661adee17ab237ad88ecc91909a835d58e9e23d
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-01-29 08:52:31 +0100

    Intro Component created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Intro from './components/intro/Intro'
 
 function App() {
-  return <div>Hello World</div>
+  return (
+    <div>
+      <Intro />
+    </div>
+  )
 }
 
 export default App
